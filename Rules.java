9bbf761b416433104ac2d5e3e6d92ea9eca9bb57
rageofpseudo <pierrejouffroy974@gmail.com>
2021-05-05 13:55:40 +0200

    Update Rules.java

--- Rules.java
+++ Rules.java
@@ -3,6 +3,53 @@
  */
 public class Rules {
 	
-
+	/**
+	*Constructeur qui initialise les valeurs initiales 
+	*
+	*/
+	Rules(){
+		
+		
+		
+	}
+	
+	// ..
+	// DeplacementMax
+	//getNumberSidesDice() --- retourne la variable global private
+	// fct loop (grid ,player  ,caractere (action))
+	
+	/**
+	*
+	*
+	*/
+	private int getNumberSidesDice(){
+		
+		
+		
+	return
+	}
+	
+	
+	/**
+	*La boucle du jeu qui choisit quel type de règle du jeu le joueur veut
+	*@param grid La grille
+	*@param player Le joueur
+	*@param cara Le type de regle que le joueur a choisit
+	*/
+	private void loop(Grid grid, Player player,String cara){
+		
+		switch(cara){
+			case 1:
+				
+			
+			
+			case 2:
+			
+			
+			
+		}
+		
+	}
+	
 
 }
